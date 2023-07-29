62f70ca83221851bdcd5171a346c0550e16314f4
loanhhquanhh <loanhhquanhh@gmail.com>
2023-07-29 10:20:06 +0700

    change

--- src/shop/components/DropDown/DropDown.js
+++ src/shop/components/DropDown/DropDown.js
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
+import Tippy from "@tippyjs/react/headless";
+import { useSpring, animated } from "@react-spring/web";
 
+const DropDown = ({ component, render, ref }) => {
 
-const DropDown = () => {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        ref.current = {
+            show: () => {
+                setShow(true);
+            },
+            hide: () => {
+                setShow(false);
+            }
+        }
+    }, [])
 
     return (
         <div>
-
+            <Tippy
+                interactive={true}
+                visible
+                render={attrs => (
+                    <animated.div
+                        {...attrs}
+                        className="w-10 h-10 bg-red-500">
+                        {render}
+                    </animated.div>
+                )}>
+                {component}
+            </Tippy>
         </div>
     );
 }
